7730e116d3192e393e3b268351631b3ac0b032f7
Koua Thao <kouathao09@gmail.com>
2018-03-13 04:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days, holiday) {
-  `It's ${days} day until ${holiday}!`
+  return `It's ${days} day until ${holiday}!`
 }
